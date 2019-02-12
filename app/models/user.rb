f76b42e65a66f63f8820f7be082b69d937792500
Asami Hashimoto <a.hashimoto.000@gmail.com>
2019-02-12 18:40:42 +0900

    Updated unfollowing_change_color

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,16 @@ class User < ApplicationRecord
     mount_uploader :image,AvatarUploader
 
     has_many :active_relationships, class_name:  "Relationship",
-    foreign_key: "follower_id",
-    dependent:   :destroy
+                foreign_key: "follower_id",
+                dependent:   :destroy
     has_many :passive_relationships, class_name:  "Relationship",
-    foreign_key: "followed_id",
-    dependent:   :destroy
+                foreign_key: "followed_id",
+                dependent:   :destroy
 
-    has_many :following, through: :active_relationships, source: :followed
-    has_many :followers, through: :passive_relationships, source: :follower
+    has_many :following, 
+                through: :active_relationships, source: :followed
+    has_many :followers,
+                 through: :passive_relationships, source: :follower
     
     def follow(other_user)
         following << other_user
