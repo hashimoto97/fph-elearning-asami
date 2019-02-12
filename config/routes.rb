beadfe8286178755c6023a0651f7fa433372758b
hashimoto97 <ko_ikeru_hondie@yahoo.co.jp>
2019-02-12 18:44:52 +0900

    Uploaded following (#9)
    * Uploaded following
    
    * Updated following_change
    
    * Updated unfollowing_change_color

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
   root 'home#index'
   get '/signup' ,to: 'users#new'
   get '/login' ,to: 'sessions#new'
   post '/login' ,to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
   resources :users,except: :new
+
+  resources :users do
+    member do
+      get :following,:followers
+    end
+  end
+
+  resources :relationships,only:[:create,:destroy]
+
 end
