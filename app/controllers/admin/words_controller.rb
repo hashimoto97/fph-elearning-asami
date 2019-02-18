f73b66011f05a5684a3012a3b092d9e5a18d9240
Asami Hashimoto <a.hashimoto.000@gmail.com>
2019-02-18 16:16:21 +0900

    Updated admin_word

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,54 @@
+class Admin::WordsController < ApplicationController
+    def index
+        @category=Category.find(params[:category_id])
+        @words = @category.words.order(created_at: :desc).paginate(page: params[:page],per_page: 10)
+    end
+
+    def new
+        @word=Word.new
+        @category=Category.find(params[:category_id])
+    end
+
+    def create
+      @category=Category.find(params[:category_id])  
+      @word=@category.words.new(word_params)
+        if @word.save
+          flash[:success]="Successfully added word!"
+          redirect_to admin_category_words_path(@category.id)
+        else
+          flash[:danger]="Word can't be blank."
+          render "new"
+        end
+    end
+
+    def edit
+        @word=Word.find(params[:id])
+        @category=Category.find(params[:category_id])
+    end
+  
+      def update
+        @word=Word.find(params[:id])
+        @category=Category.find(params[:category_id])
+        if @word.update(word_params)
+          flash[:success]="Updated word!"
+          redirect_to admin_category_words_path(@category.id)
+        else
+          flash[:danger]="Word can't be blank."
+          render "edit"
+        end
+      end
+  
+      def destroy
+          @word=Word.find(params[:id])
+          @category=Category.find(params[:category_id])
+          if @word.destroy
+            flash[:success]="Remove word."
+            redirect_to admin_category_words_path(@category.id)
+          end
+      end
+
+    private        
+        def word_params
+            params.require(:word).permit(:content)
+        end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_094007) do
+ActiveRecord::Schema.define(version: 2019_02_15_084735) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -39,4 +39,13 @@ ActiveRecord::Schema.define(version: 2019_02_13_094007) do
     t.boolean "admin", default: false
   end
 
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_words_on_category_id"
+  end
+
+  add_foreign_key "words", "categories"
 end
