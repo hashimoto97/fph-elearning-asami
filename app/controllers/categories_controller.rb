8d00aba05ae124cf482b87ef344af5f6834d1c01
Asami Hashimoto <a.hashimoto.000@gmail.com>
2019-02-27 18:08:00 +0900

    Changed_dashboard

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
+  before_action :require_login
   def index
     @categories=Category.order(created_at: :desc).paginate(page: params[:page],per_page: 10)
   end
+
+  private
+    def require_login
+      unless logged_in?
+        flash[:danger]="Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :require_login
   def index
     @user=User.find_by(id:current_user.id)
   end
+
+  private
+    def require_login
+      unless logged_in?
+        flash[:danger]="Please log in."
+        redirect_to login_url
+      end
+    end
 end
