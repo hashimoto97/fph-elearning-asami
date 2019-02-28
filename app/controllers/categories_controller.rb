f48922ca1d553a9708de0bda76614f5a41de1c61
Asami Hashimoto <a.hashimoto.000@gmail.com>
2019-02-28 18:38:22 +0900

    Changed_words_learned

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_login
   def index
-    @categories=Category.order(created_at: :desc).paginate(page: params[:page],per_page: 10)
+    @categories=Category.joins(:words).group(:id).order(created_at: :desc).paginate(page: params[:page],per_page: 10)
   end
 end
